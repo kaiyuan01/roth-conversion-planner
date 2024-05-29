acf7fa7871b1c74c1e424e4ae49e7f5ec01c9809
kaiyuan01 <kaiyuan01@users.noreply.github.com>
2024-05-29 22:24:18 +0000

    add most of data g=fields and preliminary calculation based on some user inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,13 @@ import { ReactiveFormsModule } from '@angular/forms';
 export class AppComponent implements OnInit  {
   title = 'Roth Conversion Planner';
 
+  INIT_DED = '29,200';
+  INIT_BAL = '1042697.98';
+  INIT_RET = '10%';
+  INIT_INC = '16,338';
+  INIT_TB = '22%';
+
+
   myForm : FormGroup = new FormGroup({
     bal: new FormControl('', Validators.required),
     email: new FormControl('', [Validators.required, Validators.email]),
@@ -24,20 +31,29 @@ export class AppComponent implements OnInit  {
 
   ngOnInit() {
     this.myForm = new FormGroup({
-      bal: new FormControl('', Validators.required),
-      return: new FormControl('', [Validators.required]),
+      bal: new FormControl(this.INIT_BAL, Validators.required),
+      return: new FormControl(this.INIT_RET, [Validators.required]),
       age: new FormControl('', Validators.required),
       
-      income: new FormControl('', [Validators.required]),
-      deductible: new FormControl('', [Validators.required]),
+      income: new FormControl(this.INIT_INC, [Validators.required]),
+      deductible: new FormControl(this.INIT_DED, [Validators.required]),
       conversion: new FormControl('', Validators.required),
       taxableIncome: new FormControl('', Validators.required),
+      taxBracket: new FormControl(this.INIT_TB, Validators.required),
     });
+
+    
+
   }
 
   onSubmit() {
     console.log(this.myForm.value);
     // You can add code here to submit the form data to a server or do something else with it
+
+    if(this.myForm && this.myForm.get('taxBracket') && this.myForm.get('taxBracket')?.value == "22%") {
+      this.myForm.get("taxableIncome")?.setValue( '201050' );
+      this.myForm.get("conversion")?.setValue( eval( this.myForm.get("taxableIncome")?.value - this.myForm.get("income")?.value + this.myForm.get("deductible")?.value ) );
+    }
   }
 
 }
