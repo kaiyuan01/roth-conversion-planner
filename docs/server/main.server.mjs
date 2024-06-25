994fc558d938fbeb296f4e3cdb3a886ec52e06cd
kaiyuan01 <kaiyuan01@users.noreply.github.com>
2024-06-25 15:18:52 +0000

    releasing 1.0.10beata

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-Q77SA6UP.mjs";import"./chunk-2FDVY5N7.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-ALSKHCP2.mjs";import"./chunk-2FDVY5N7.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
